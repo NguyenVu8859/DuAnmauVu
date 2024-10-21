4ee7500d3e04213e5ea6db88a235010bd4571600
DawnMinhHuy <minhhuygo012304@gmail.com>
2024-10-21 21:21:01 +0700

    Duan MangXaHoiVaDuLich

---
+++ src/main/java/com/blogdulich/DuAnTotNghiepMangXaHoiVaDuLichApplication.java
@@ -0,0 +1,13 @@
+package com.blogdulich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnTotNghiepMangXaHoiVaDuLichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnTotNghiepMangXaHoiVaDuLichApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogdulich/DuAnTotNghiepMangXaHoiVaDuLichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogdulich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnTotNghiepMangXaHoiVaDuLichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
